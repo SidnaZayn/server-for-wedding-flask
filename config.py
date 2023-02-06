ed8646a658ff3330e137b3189c470ed824b68e39
zettravos321 <sidna.zen@imaniprima.com>
2023-02-07 00:56:53 +0700

    test

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ def load_configuration_from_json(_json_filepath = SETTINGS_FILE_NAME):
     """Load the configuration from the settings.json file"""
     global CONF_DICT
     try:
-        with open(_json_filepath,'rb') as conf_file:
+        with open(_json_filepath,'r') as conf_file:
             CONF_DICT = json.load(conf_file)['config']
 
     except Exception as e:
@@ -19,53 +19,42 @@ def load_configuration_from_json(_json_filepath = SETTINGS_FILE_NAME):
 
 class Config(object):
     """Common generic configurations"""
-    ## Define the application directory
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-    ## Load configuration details from settings.json file
-    load_configuration_from_json(os.path.join(BASE_DIR, SETTINGS_FILE_NAME))
+    # ## Define the application directory
+    # BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    #
+    # ## Load configuration details from settings.json file
+    # load_configuration_from_json(os.path.join(BASE_DIR, SETTINGS_FILE_NAME))
 
     ## HOST & PORT
-    HOST = CONF_DICT['common']['HOST']
-    PORT = CONF_DICT['common']['PORT']
+    HOST = '127.0.0.1'
+    PORT = 8008
 
     ## Version
-    VERSION = CONF_DICT['common']['VERSION']
+    VERSION = 1.0
 
     ## URL Prefix
-    URL_PREFIX = CONF_DICT['common']['URL_PREFIX']
+    URL_PREFIX = "api"
 
     ## Statement for enabling the development environment
-    DEBUG = CONF_DICT['common']['DEFAULT_DEBUG']
+    DEBUG = "true"
 
     ## Application threads
-    THREADS_PER_PAGE = CONF_DICT['common']['THREADS_PER_PAGE']
+    THREADS_PER_PAGE = 2
 
     ## Enable protection against *Cross-site Request Forgery (CSRF)*
-    CSRF_ENABLED = CONF_DICT['common']['CSRF_ENABLED']
-    CSRF_SESSION_KEY = CONF_DICT['common']['CSRF_SESSION_KEY']
-
-
-class ProductionConfig(Config):
-    '''
-    Configuration specific to production environment
-    '''
-    ENV = CONF_DICT['env']['production']['ENV']
-    DEBUG = CONF_DICT['env']['production']['DEBUG']
-    DEVELOPMENT = CONF_DICT['env']['production']['DEVELOPMENT']
-    CONNECT_TIMEOUT = CONF_DICT['env']['development']['DATABASE_CONNECTION_OPTIONS']['CONNECT_TIMEOUT']
-
+    CSRF_ENABLED = "true"
+    CSRF_SESSION_KEY = ""
 
 class DevelopmentConfig(Config):
     '''
     Configuration specific to development environment
     '''
-    ENV = CONF_DICT['env']['development']['ENV']
-    DEBUG = CONF_DICT['env']['development']['DEBUG']
-    DEVELOPMENT = CONF_DICT['env']['development']['DEVELOPMENT']
-    DB_HOST = CONF_DICT['env']['development']['DATABASE_CONNECTION_OPTIONS']['DB_HOST']
-    DB_PORT = CONF_DICT['env']['development']['DATABASE_CONNECTION_OPTIONS']['DB_PORT']
-    DB_USER = CONF_DICT['env']['development']['DATABASE_CONNECTION_OPTIONS']['DB_USER']
-    DB_PASSWD = CONF_DICT['env']['development']['DATABASE_CONNECTION_OPTIONS']['DB_PASSWD']
-    DB_NAME = CONF_DICT['env']['development']['DATABASE_CONNECTION_OPTIONS']['DB_NAME']
-    CONNECT_TIMEOUT = CONF_DICT['env']['development']['DATABASE_CONNECTION_OPTIONS']['CONNECT_TIMEOUT']
+    ENV = "development"
+    DEBUG = "true"
+    DEVELOPMENT = "true"
+    DB_HOST = "127.0.0.1"
+    DB_PORT = 3306
+    DB_USER = "undanga4_zen"
+    DB_PASSWD = "ft,_DY3p.Ap!"
+    DB_NAME = "undanga4_wedding"
+    CONNECT_TIMEOUT = 5
