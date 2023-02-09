ba6c2ea22711e9b3e4729c3ace2909bfd3d75904
zenimaniprima <sidna.zen@imaniprima.com>
2023-02-09 23:51:52 +0700

    fix bug

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def delete_data_satu_tamu():
             response = get_response_msg(records, HTTPStatus.BAD_REQUEST)
             return response
         if connection_object.is_connected():
-            cursor.execute(f"DELETE FROM tb_guests WHERE id={id}")
+            cursor.execute(f"DELETE FROM tb_guests WHERE id='{id}'")
             print("id yang dihapus adalah : " + str(id) + " pada:" + x)
     except Error as e:
         print("Error while connecting to MySQL using Connection pool ", e)
